242d841edc58828d39b32f75ed923e18998a1d8e
Matt Joseph <mattomjoe@yahoo.com>
2019-10-26 13:00:48 -0500

    Saturday class updates.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,6 +3,9 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
+    console.log("---------Serving up root from htmlRoutes.js");
+    //console.log("----------Hey is this you my dude?: ",req.userContext.userinfo);
+
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   const { userContext } = req;
+
+  console.log("----------Hey is this you my dude?: ", req.userContext.userinfo);
+
   res.render("index", { userContext });
 });
 
